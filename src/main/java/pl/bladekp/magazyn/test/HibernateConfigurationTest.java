5652096c1a96a0e7d8fad6a9bc88ab83e2003279
bladekp <bladekp@localhost.localdomain>
2016-10-26 21:43:04 +0200

    Simple test for hibernate

---
+++ src/main/java/pl/bladekp/magazyn/test/HibernateConfigurationTest.java
@@ -0,0 +1,30 @@
+package pl.bladekp.magazyn.test;
+
+import org.hibernate.Session;
+import pl.bladekp.magazyn.model.Parameter;
+import pl.bladekp.magazyn.util.HibernateUtil;
+
+public class HibernateConfigurationTest {
+    public static void main(String args[]){
+        Parameter p = new Parameter();
+        p.setName("Current");
+        p.setUnit("I");
+        p.setMeasure("A");
+        p.setDescription("Current");
+        
+        //creating
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(p);
+        session.getTransaction().commit();
+        System.out.println("Parameter " + p.getName() + " ID: " + p.getId());
+        
+        //deleting
+        session.beginTransaction();
+        session.delete(p);
+        session.getTransaction().commit();
+        
+        //closing session
+        HibernateUtil.getSessionFactory().close();
+    }
+}
