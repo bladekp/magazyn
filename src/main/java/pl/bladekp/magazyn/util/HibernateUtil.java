618f86eeaa7bb81e4b3b347fff0a617bd312e43e
bladekp <bladekp@localhost.localdomain>
2016-10-26 21:42:40 +0200

    changing SessionFactory creating method

--- src/main/java/pl/bladekp/magazyn/util/HibernateUtil.java
+++ src/main/java/pl/bladekp/magazyn/util/HibernateUtil.java
@@ -1,7 +1,9 @@
 package pl.bladekp.magazyn.util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 /**
  *
  * @author bladekp
@@ -11,13 +13,16 @@ public class HibernateUtil {
     private static final SessionFactory sessionFactory;
     
     static {
-        System.out.println("static");
-        try {
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+			.configure()
+			.build();
+	try {
+		sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+	}
+	catch (Exception e) {
+		StandardServiceRegistryBuilder.destroy( registry );
+                throw e;
+	}
     }
     
     public static SessionFactory getSessionFactory() {
